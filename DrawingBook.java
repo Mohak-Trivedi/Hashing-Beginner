8c38d1d763c076ca87399b5f1c28f63c60335201
Mohak-Trivedi <trivedimohak@gmail.com>
2023-08-29 21:37:22 +0530

    completed Drawing Book with HashMap

--- DrawingBook.java
+++ DrawingBook.java
@@ -23,40 +23,57 @@ class Result {
      * 2. INTEGER p
      */
     // Approach: w/o HashMap T.C.: O(2n) S.C.: O(1)
+    // public static int pageCount(int n, int p) {
+    // // Write your code
+    // if (n % 2 == 0) {
+    // if (p == 1 || p == n) {
+    // return 0;
+    // }
+    // } else {
+    // if (p == 1 || p == n || p == n - 1) {
+    // return 0;
+    // }
+    // }
+
+    // int currPage = 2; // already searched at 1 above
+    // int turnedFromOne = 1; // as we are starting from Page 2, 1 is already turned
+    // while ((p != currPage) && (p != currPage + 1)) {
+    // currPage += 2;
+    // turnedFromOne++;
+    // }
+
+    // int turnedFromLast;
+    // if (n % 2 == 0) {
+    // currPage = n - 1;
+    // } else {
+    // currPage = n - 2;
+    // }
+    // turnedFromLast = 1;
+    // while ((p != currPage) && (p != currPage - 1)) {
+    // currPage -= 2;
+    // turnedFromLast++;
+    // }
+
+    // return Math.min(turnedFromOne, turnedFromLast);
+    // }
+
+    // Approach: w/ HashMap T.C.: O(n) S.C.:O(n)
     public static int pageCount(int n, int p) {
         // Write your code
-        if (n % 2 == 0) {
-            if (p == 1 || p == n) {
-                return 0;
+        int pagesTurned = 0;
+        Map<Integer, Integer> hmap = new HashMap<>();
+        for (int currPage = 0; currPage <= n; currPage++) {
+            hmap.put(currPage, pagesTurned);
+            currPage++;
+            if (currPage <= n) {
+                hmap.put(currPage, pagesTurned);
             }
-        } else {
-            if (p == 1 || p == n || p == n - 1) {
-                return 0;
-            }
+            pagesTurned++;
         }
-
-        int currPage = 2; // already searched at 1 above
-        int turnedFromOne = 1; // as we are starting from Page 2, 1 is already turned
-        while ((p != currPage) && (p != currPage + 1)) {
-            currPage += 2;
-            turnedFromOne++;
-        }
-
-        int turnedFromLast;
-        if (n % 2 == 0) {
-            currPage = n - 1;
-        } else {
-            currPage = n - 2;
-        }
-        turnedFromLast = 1;
-        while ((p != currPage) && (p != currPage - 1)) {
-            currPage -= 2;
-            turnedFromLast++;
-        }
-
-        return Math.min(turnedFromOne, turnedFromLast);
+        int pagesTurnedFromFirst = hmap.get(p);
+        int pagesTurnedFromLast = hmap.get(n) - hmap.get(p);
+        return Math.min(pagesTurnedFromFirst, pagesTurnedFromLast);
     }
-
 }
 
 // Use class name Solution in HackerRank
