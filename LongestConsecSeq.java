7b1b19455cd19473f8d312a332cf69835b9e718d
Mohak-Trivedi <trivedimohak@gmail.com>
2023-11-01 13:25:06 +0530

    Solved Longest Consecutive Sequence without Hashing

--- LongestConsecSeq.java
+++ LongestConsecSeq.java
@@ -15,23 +15,28 @@ class Solution {
         int maxCnt = 0;
 
         int idx = 0;
-        while (idx < nums.length) {
-            int cnt = 1;
-            while (idx < nums.length - 1) {
-                if (nums[idx + 1] == (nums[idx] + 1)) {
-                    cnt++;
-                    idx++;
-                } else if (nums[idx + 1] == nums[idx]) {
-                    idx++;
-                } else {
-                    maxCnt = Math.max(maxCnt, cnt);
-                    cnt = 1;
-                    idx++;
-                }
+
+        int cnt = 1;
+        while (idx < nums.length - 1) {
+            if (nums[idx + 1] == (nums[idx] + 1)) {
+                cnt++;
+                idx++;
+            } else if (nums[idx + 1] == nums[idx]) { // need not count, but need to include equal consecutive elements
+                                                     // in the sequence
+                idx++;
+            } else {
+                maxCnt = Math.max(maxCnt, cnt);
+                cnt = 1;
+                idx++;
             }
-            maxCnt = Math.max(maxCnt, cnt);
-            idx++;
         }
+        maxCnt = Math.max(maxCnt, cnt); // Important in case else never ran -> maxCnt never got updated -> entire array
+                                        // might be a sequence. And, even if the else ran the best maxCnt will be
+                                        // retained due to Math.max. Think: [1,2,0,1]
+
+        // You might think, why not create a separate case if(maxCnt == 0) maxCnt =
+        // nums.length; But we can't do that as we must not include count of equal
+        // elements in the sequence.
 
         return maxCnt;
     }
